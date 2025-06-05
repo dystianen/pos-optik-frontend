62b6423a92750c17bee82c97a4655cc042fcc949
Dystian En <distian.enyus@gmail.com>
2025-06-05 15:01:18 +0700

    fix: header link

--- src/components/Layout/Header/Navigation/HeaderLink.tsx
+++ src/components/Layout/Header/Navigation/HeaderLink.tsx
@@ -34,7 +34,7 @@ const HeaderLink: React.FC<{ item: HeaderItem }> = ({ item }) => {
         href={`/${item.href}`}
         className={`text-lg flex hover:text-black capitalized relative ${
           isActive
-            ? 'text-black after:absolute after:w-24 after:h-1 after:bg-primary after:rounded-full after:-bottom-1'
+            ? 'text-black after:absolute after:w-8 after:h-1 after:bg-primary after:rounded-full after:-bottom-1'
             : 'text-grey'
         }`}
       >
--- src/components/Layout/Header/Navigation/menuData.tsx
+++ src/components/Layout/Header/Navigation/menuData.tsx
@@ -1,7 +1,7 @@
 import { HeaderItem } from '@/types/menu'
 
 export const headerData: HeaderItem[] = [
-  { label: 'Frame Kacamata', href: '/' },
+  { label: 'Frame Kacamata', href: '/#frame' },
   { label: 'Lensa Kacamata', href: '/#courses' },
   { label: 'Softlens', href: '/#mentor' },
   { label: 'Aksesoris', href: '/#portfolio' }
